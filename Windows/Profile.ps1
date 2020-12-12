a2363bfa156baee971b965d00887d8e4a4f96221
ustcw0ng <ustcw0ng@mail.ustc.edu.cn>
2020-12-12 11:05:33 +0800

    update

--- Windows/Profile.ps1
+++ Windows/Profile.ps1
@@ -5,49 +5,58 @@
 foreach($_ in Get-Content -Path C:\Users\90834\Documents\env.txt) 
 { if ($_ -match '^([^=]+)=(.*)') 
 { [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2]) } }
+if (Test-Path F:\) {
+    Import-Module 'F:\vcpkg\vcpkg\scripts\posh-vcpkg'
+}
 
-#set python env
-New-Alias -Name python3 -Value py.exe
-New-Alias -Name python -Value py.exe
+#Set python env
+Set-Alias -Name python3 -Value py.exe
+Set-Alias -Name python -Value py.exe
+
+#Set winget autocompletion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
 
 #Set autocompletion
 Import-Module PSReadLine
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt jandedobbeleer
-#Set-PoshPrompt aliens
 
-#Set-PSReadlineOption -EditMode vi
 Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
 #Edit Hosts
 Function Edit-Hosts {
-    Start-Process code -ArgumentList $env:windir\System32\drivers\etc\hosts -Wait
+    Start-Process code -ArgumentList $env:windir\System32\drivers\etc\hosts -Wait -NoNewWindow
     ipconfig /flushdns | Out-Null
 }
-
 function Update-All {
     Start-Job -Name "Visual Studio update"{
-        &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer" update `
-            --quiet --installpath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" }
+        & $env:VSINSTALLDIR\..\..\Installer\vs_installer.exe update `
+            --quiet --installpath $env:VSINSTALLDIR }
     Start-Job -Name "office update"{
         &"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient" `
             /update user displaylevel=false forceappshutdown=true }
-    Start-Job -Name "vcpkg update"{ 
-        if (Set-Location "F:\vcpkg\vcpkg") {
-            git pull | Out-Null
-            vcpkg update
-        } }
+    if (Test-Path F:\) {
+        git -C "F:\vcpkg\vcpkg" pull | Out-Null
+        vcpkg update
+    } 
     Start-Process ms-settings:windowsupdate-action
     Start-Process ms-windows-store://downloadsandupdates
-    Write-Output "WSL update ..."
-    wsl sudo apt update '&&' sudo apt upgrade
+    #Write-Output "WSL update ..."
+    #wsl sudo apt update '&&' sudo apt upgrade
     Write-Output "Git update ..."
     git update-git-for-windows
     Write-Output "Powershell module update"
-    Update-Module
+    Update-Module 
     Write-Output "pip update"
     $a = pip list --outdated
     $num_package = $a.Length - 2
@@ -56,26 +65,7 @@ function Update-All {
         pip install -U $tmp
     }
 }
-
 function Remove-DS-Store {
        Get-ChildItem . -r -include ._* -force | remove-item -r -force
        Get-ChildItem . -r -include .DS_Store -force | remove-item -r -force
-   Write-Output "Delete success"
-}
-
-function Update-PowerShell {
-    Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
-}
-
-function calculator {
-    ipython -c "from math import *; from numpy import *; from scipy import *; from sympy import *"
-}
-
-function Update-VC-env {
-    Start-Process powershell.exe -ArgumentList "-NoProfile -File C:\Users\90834\Documents\Utilities\Windows\Update-VC-env.ps1" `
-    -UseNewEnvironment -Wait -NoNewWindow
-}
-
-
-
-
+   Write-O
