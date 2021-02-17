de78378d4f6d52f72f7fc47e0a2e6e48e951ee3a
ustcw0ng <ustcw0ng@mail.ustc.edu.cn>
2021-02-17 11:07:57 +0800

    argument

--- Windows/Profile.ps1
+++ Windows/Profile.ps1
@@ -1,9 +1,10 @@
-#Set python env
+#不要用shouldprocess
+#设置python环境
 Function Update-Pip {
     pip list --outdated | Select-Object -Skip 2 | ForEach-Object { pip install -U $_.Remove($_.IndexOf(' ')) }
 }
 
-#Set autocompletion
+#设置自动补全
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
     [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
@@ -18,15 +19,15 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt jandedobbeleer
 
-#Set readline
+#设置PSreadline
 Import-Module PSReadLine
 Function OnViModeChange {
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a blinking block.
+    if ($args[0] -ceq 'Command') {
+        # set the cursor to a blinking block.
         Write-Host -NoNewline "`e[1 q"
     }
     else {
-        # Set the cursor to a blinking line.
+        # set the cursor to a blinking line.
         Write-Host -NoNewline "`e[5 q"
     }
 }
@@ -34,44 +35,43 @@ Set-PSReadLineOption -EditMode Vi -ViModeIndicator Script -ViModeChangeHandler $
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+if ($env:TERM_PROGRAM -ceq "vscode") { Write-Host -NoNewline "`e[5 q" } 
 
 Function Edit-Hosts {
     code $env:windir\System32\drivers\etc\hosts --wait && Clear-DnsClientCache | Out-Null
 }
 Function Update-All {
-    if ($args.Count -eq 0) {
-        Start-Job -Name "office update" {
-            &"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient" `
-                /update user displaylevel=false forceappshutdown=true }
-    }
+    Start-Job -Name "office升级" {
+        &"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient" `
+            /update user displaylevel=false forceappshutdown=true }
     Start-Process ms-settings:windowsupdate-action
     Start-Process ms-windows-store://downloadsandupdates
-    Write-Output "Powershell module update"
+    Write-Output "powershell module升级"
     Update-Module && Update-Module -Name oh-my-posh -Scope CurrentUser -AllowPrerelease
-    Write-Output "pip update"
+    Write-Output "pip升级"
     Update-Pip
     if (Test-Path F:\) {
-        Write-Output "vcpkg update"
+        Write-Output "vcpkg升级"
         git -C "F:\vcpkg\vcpkg" pull #| Out-Null
         vcpkg update
-        Write-Output "texlive update"
+        Write-Output "texlive升级"
         tlmgr update --self --all
     }
 }
 Function Remove-DS-Store {
-    Get-ChildItem $args -Recurse -Include ._*, .DS_Store -Force | Remove-Item -Force -Verbose
+    Get-ChildItem -Recurse -Include ._*, .DS_Store -Force | Remove-Item @args
 }
 Function Calculator {
-    ipython -c "from math import *; from numpy import *; from scipy import *; from sympy import *" $args
+    ipython -c "from math import *; from numpy import *; from scipy import *; from sympy import *" @args
 }
-Function Set-HTTP-Proxy {
-    $env:HTTP_PROXY = $env:HTTPS_PROXY = $args.Count -eq 0 ? "http://127.0.0.1:10809" : $args[0]
+Function set-HTTP-Proxy([string]$Server = "http://127.0.0.1:10809") {
+    $env:HTTP_PROXY = $env:HTTPS_PROXY = $Server
 }
 Function Get-GNU-Date {
-    #need chinese locale
+    #需要中文locale
     $str = (Get-Date -Format "yyyy年MM月dd日 dddd HH时mm分ss秒 CST").ToCharArray()
-    if ($str[5] -eq '0') { $str[5] = ' ' }
-    if ($str[8] -eq '0') { $str[8] = ' ' }
+    if ($str[5] -ceq '0') { $str[5] = ' ' }
+    if ($str[8] -ceq '0') { $str[8] = ' ' }
     -join $str
 }
 Function Set-VC-env {
@@ -80,16 +80,16 @@ Function Set-VC-env {
         -DevCmdArguments "-arch=x64 -host_arch=x64" -SkipAutomaticLocation
 }
 Function Get-ChildSize {
-    Get-ChildItem $args -Force |
+    Get-ChildItem @args -Force |
     ForEach-Object -Parallel {
-        #Onedrive is a reparsepoint, but not a symlink, if you want to onedrive passes
-        #we can change this to $_.Linktype -eq "Junction","Symlink"
+        #Onedrive为reparsepoint，却非symlink，若需改
+        #改之为$_.Linktype -ceq "Junction","SymLink?"
         try {   
             $LengthOrTarget = $_.Attributes -band [System.IO.FileAttributes]::ReparsePoint ? $_.Target :
             -not ($_.Attributes -band [System.IO.FileAttributes]::Directory) ? $_.Length:
-            (Get-ChildItem $_ -Recurse -Force -File -ErrorAction:Stop | Measure-Object -Sum Length).Sum 
+            (Get-ChildItem $_ -Recurse -Force -File -ErrorAction Stop | Measure-Object -Sum Length).Sum 
         }
-        catch { $LengthOrTarget = $Error[0] }
+        catch [System.UnauthorizedAccessException] { $LengthOrTarget = "`e[31mAccess Denied" }
         $_ | Select-Object Mode, LastWriteTime, @{Name = "LengthOrTarget"; Expression = { $LengthOrTarget } }, Name
     }
 }
@@ -114,10 +114,10 @@ foreach ($_ in Get-Content -Path $HOME\Documents\env.txt) {
     if ($_ -match '^([^=]+)=(.*)')
     { [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2]) }
 }
-    Write-Output "WSL update ..."
+    Write-Output "WSL升级 ..."
     wsl sudo apt update '&&' sudo apt upgrade
-    Write-Output "Git update ..."
+    Write-Output "Git升级 ..."
     git update-git-for-windows
-    Write-Output "Rust update"
+    Write-Output "Rust升级"
     rustup self update && rustup update
 #>
